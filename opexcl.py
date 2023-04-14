8762a6d35f2a6bb11acf235300873861fe8489ed
Oussama Hmoura <hmouraoussama@gmail.com>
2023-04-14 15:29:30 +0000

    Pdf copy

--- opexcl.py
+++ opexcl.py
@@ -1,6 +1,6 @@
 import openpyxl
 import os
-import re
+import shutil
 
 handle = '123456789/'
 collection = '123456789/2'
@@ -41,14 +41,7 @@ for row in worksheet.iter_rows(min_row=2, values_only=True):
         elif column_name == 'Encadrants':
 
             record_meta['encadrants'] = row[5]
-    try:
-        link = worksheet.cell(row=linkNumbers, column=1).hyperlink.target
-        linkes = worksheet.cell(row=linkNumbers, column=1).value
-        links.append(link.replace('../../Sauvegarde/', ''))
-    except AttributeError:
-        pass
 
-    linkNumbers += 1
     records.append(record)
     records_meta.append(record_meta)
 
@@ -58,7 +51,12 @@ for record in records:
     xml_string = '<?xml version="1.0" encoding="utf-8" standalone="no"?>\n'
     xml_string += '<dublin_core schema="dc">\n'
     title = record['title']
-
+    link = None
+    try:
+        link = worksheet.cell(row=number+1, column=1).hyperlink.target
+        linkes = worksheet.cell(row=number+1, column=1).value
+    except AttributeError:
+        pass
     xml_string += f'<dcvalue element="identifier" qualifier="uri">https:&#x2F;&#x2F;sbn.inpt.ac.ma&#x2F;handle&#x2F;123456789&#x2F;{title}</dcvalue>\n'
     for key, value in record.items():
         xml_string += f'<dcvalue element="{key}" qualifier="none">{value}</dcvalue>\n'
@@ -69,6 +67,16 @@ for record in records:
         f.write(collection)
     with open(f'./csv/{number}/handle', 'w', encoding='utf-8') as f:
         f.write(handle+str(number))
+
+    if link is not None:
+        link = link.replace('../../Sauvegarde/', '')
+        link_import = link.replace('PFE 1999/', '')
+        text_import = f'{link_import}	bundle:ORIGINAL'
+        with open(f'./csv/{number}/contents', 'w', encoding='utf-8') as f:
+            f.write(text_import)
+
+        shutil.copy(f'./PFE/{link}', f'./csv/{number}/')
+
     number += 1
 
 
@@ -89,6 +97,3 @@ for record in records_meta:
     with open(f'./csv/{number2}/metadata_pfe.xml', 'w', encoding='utf-8') as f:
         f.write(xml_string)
     number2 += 1
-
-print(links)
-print(len(links))

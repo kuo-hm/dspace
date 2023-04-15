0c8cc32b8303836c5b74daa8c3280fe9b2e280d6
Oussama Hmoura <hmouraoussama@gmail.com>
2023-04-15 15:57:37 +0000

    Added years loop

--- opexcl.py
+++ opexcl.py
@@ -2,98 +2,109 @@ import openpyxl
 import os
 import shutil
 
-handle = '123456789/'
-collection = '123456789/2'
 
+def create_dir(year, number, number2):
 
-workbook = openpyxl.load_workbook('./liste-globale-des-PFE.xlsx')
-worksheet = workbook['PFE 1999']
+    handle = '123456789/'
+    collection = '123456789/2'
 
-column_indices = {}
-for column in worksheet.iter_cols(min_row=1, max_row=1, values_only=True):
-    for index, cell_value in enumerate(column):
-        column_indices[cell_value] = index
+    print('Creating directory for year: ', year)
 
-records = []
-records_meta = []
-links = []
+    workbook = openpyxl.load_workbook('./liste-globale-des-PFE.xlsx')
+    worksheet = workbook['PFE '+str(year)]
+
+    column_indices = {}
+
+    for column in worksheet.iter_cols(min_row=1, max_row=1, values_only=True):
+        for index, cell_value in enumerate(column):
+            column_indices[cell_value] = index
+
+    records = []
+    records_meta = []
+
+    for row in worksheet.iter_rows(min_row=2, values_only=True):
+        record = {}
+        record_meta = {}
+        for column_name, column_index in column_indices.items():
+
+            if column_name == 'Titre':
+                record['title'] = row[0]
+
+            elif column_name == 'Auteur':
+                record['publisher'] = row[1]
+            elif column_name == 'Sujet':
+                record['description'] = row[2]
+            elif column_name == 'Date':
+                record['date'] = row[3]
+            elif column_name == 'Organisme':
+                record_meta['organisme'] = row[4]
+            elif column_name == 'Encadrants':
+
+                record_meta['encadrants'] = row[5]
+
+        records.append(record)
+        records_meta.append(record_meta)
+    for record in records:
+        os.makedirs(f'./csv/{year}/{number}', exist_ok=True)
+
+        xml_string = '<?xml version="1.0" encoding="utf-8" standalone="no"?>\n'
+        xml_string += '<dublin_core schema="dc">\n'
+        title = record['title']
+        link = None
+        try:
+            link = worksheet.cell(row=number+1, column=1).hyperlink.target
+            linkes = worksheet.cell(row=number+1, column=1).value
+        except AttributeError:
+            pass
+        xml_string += f'<dcvalue element="identifier" qualifier="uri">https:&#x2F;&#x2F;sbn.inpt.ac.ma&#x2F;handle&#x2F;123456789&#x2F;{title}</dcvalue>\n'
+        for key, value in record.items():
+            xml_string += f'<dcvalue element="{key}" qualifier="none">{value}</dcvalue>\n'
+        xml_string += '</dublin_core>'
+        with open(f'./csv/{year}/{number}/dublin_core.xml', 'w', encoding='utf-8') as f:
+            f.write(xml_string)
+        with open(f'./csv/{year}/{number}/collections', 'w', encoding='utf-8') as f:
+            f.write(collection)
+        with open(f'./csv/{year}/{number}/handle', 'w', encoding='utf-8') as f:
+            f.write(handle+str(number))
+
+        if link is not None:
+            link = link.replace('../../Sauvegarde/', '')
+            link_import = link.replace('PFE '+str(year)+'/', '')
+            text_import = f'{link_import}	bundle:ORIGINAL'
+            with open(f'./csv/{year}/{number}/contents', 'w', encoding='utf-8') as f:
+                f.write(text_import)
+
+            shutil.copy(f'./PFE/{link}', f'./csv/{year}/{number}/')
+
+        number += 1
+
+    for record in records_meta:
+        xml_string = '<dublin_core schema="pfe">\n'
+        for key, value in record.items():
+            if key == 'encadrants':
+                if value is None:
+                    value = ''
+
+                name_list = value.split('\n')
+                for name in name_list:
+                    text = name.strip()
+                    xml_string += f'<dcvalue element="{key}" qualifier="none">{text}</dcvalue>\n'
+            else:
+                xml_string += f'<dcvalue element="{key}" qualifier="none">{value}</dcvalue>\n'
+        xml_string += '</dublin_core>'
+        with open(f'./csv/{year}/{number2}/metadata_pfe.xml', 'w', encoding='utf-8') as f:
+            f.write(xml_string)
+        number2 += 1
+    return number, number2
+
 
 number = 1
-linkNumbers = 2
 number2 = 1
 
-for row in worksheet.iter_rows(min_row=2, values_only=True):
-    record = {}
-    record_meta = {}
-    for column_name, column_index in column_indices.items():
-
-        if column_name == 'Titre':
-            record['title'] = row[0]
-
-        elif column_name == 'Auteur':
-            record['publisher'] = row[1]
-        elif column_name == 'Sujet':
-            record['description'] = row[2]
-        elif column_name == 'Date':
-            record['date'] = row[3]
-        elif column_name == 'Organisme':
-            record_meta['organisme'] = row[4]
-        elif column_name == 'Encadrants':
-
-            record_meta['encadrants'] = row[5]
-
-    records.append(record)
-    records_meta.append(record_meta)
-
-for record in records:
-    os.makedirs(f'./csv/{number}', exist_ok=True)
-
-    xml_string = '<?xml version="1.0" encoding="utf-8" standalone="no"?>\n'
-    xml_string += '<dublin_core schema="dc">\n'
-    title = record['title']
-    link = None
-    try:
-        link = worksheet.cell(row=number+1, column=1).hyperlink.target
-        linkes = worksheet.cell(row=number+1, column=1).value
-    except AttributeError:
-        pass
-    xml_string += f'<dcvalue element="identifier" qualifier="uri">https:&#x2F;&#x2F;sbn.inpt.ac.ma&#x2F;handle&#x2F;123456789&#x2F;{title}</dcvalue>\n'
-    for key, value in record.items():
-        xml_string += f'<dcvalue element="{key}" qualifier="none">{value}</dcvalue>\n'
-    xml_string += '</dublin_core>'
-    with open(f'./csv/{number}/dublin_core.xml', 'w', encoding='utf-8') as f:
-        f.write(xml_string)
-    with open(f'./csv/{number}/collections', 'w', encoding='utf-8') as f:
-        f.write(collection)
-    with open(f'./csv/{number}/handle', 'w', encoding='utf-8') as f:
-        f.write(handle+str(number))
-
-    if link is not None:
-        link = link.replace('../../Sauvegarde/', '')
-        link_import = link.replace('PFE 1999/', '')
-        text_import = f'{link_import}	bundle:ORIGINAL'
-        with open(f'./csv/{number}/contents', 'w', encoding='utf-8') as f:
-            f.write(text_import)
-
-        shutil.copy(f'./PFE/{link}', f'./csv/{number}/')
-
-    number += 1
-
-
-for record in records_meta:
-    xml_string = '<dublin_core schema="pfe">\n'
-    for key, value in record.items():
-        if key == 'encadrants':
-            if value is None:
-                value = ''
-
-            name_list = value.split('\n')
-            for name in name_list:
-                text = name.strip()
-                xml_string += f'<dcvalue element="{key}" qualifier="none">{text}</dcvalue>\n'
-        else:
-            xml_string += f'<dcvalue element="{key}" qualifier="none">{value}</dcvalue>\n'
-    xml_string += '</dublin_core>'
-    with open(f'./csv/{number2}/metadata_pfe.xml', 'w', encoding='utf-8') as f:
-        f.write(xml_string)
-    number2 += 1
+# years from 1999 to 2020
+years = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008,
+         2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+for year in years:
+    number, number2 = create_dir(year, number, number2)
+    print('Done for year: ', year)
+    print('----------------------------------------')
